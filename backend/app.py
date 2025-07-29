a7a41089e9708e4beed35298040d59ceb3f8614e
Lucas <lucas.bresson25@gmail.com>
2025-07-29 17:23:38 +0200

    Ajout du frontend React

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from logic import tirer_cartes
-import random
-from cartes import charger_cartes, tirer_cartes, remplacer_carte, get_themes # Ou adapter selon ton fichier de données
-
+from cartes import charger_cartes, tirer_cartes
 
 app = Flask(__name__)
 CORS(app)  # Autorise les appels depuis le frontend React
@@ -11,12 +8,6 @@ CORS(app)  # Autorise les appels depuis le frontend React
 @app.route("/")
 def home():
     return "API Pake de Cartes fonctionne !"
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
-app = Flask(__name__)
-CORS(app)
 
 @app.route("/tirage", methods=["POST"])
 def tirage():
@@ -32,3 +23,6 @@ def tirage():
     except Exception as e:
         print(f"Erreur dans /tirage : {e}")
         return jsonify({"error": "Erreur serveur"}), 500
+
+if __name__ == "__main__":
+    app.run(debug=True)
